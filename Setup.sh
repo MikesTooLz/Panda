45cffe4a658d44ba6a385c74ff0c8cc74c2a124a
MikesTooLz <github.track@mikestoolz.com>
2023-10-08 13:59:32 -0400

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 apt-get update
 apt-get install dfu-util gcc-arm-none-eabi python3-pip libffi-dev git scons screen
+sleep 5
 git clone https://github.com/commaai/panda.git
+sleep 5
 cd panda
 pip install -r requirements.txt
+sleep 5
 python setup.py install
+sleep 5
 grep SUBSYS README.md | sudo tee /etc/udev/rules.d/11-panda.rules
 udevadm control --reload-rules && sudo udevadm trigger
 lsusb
 cd board
 sudo scons -u
+sleep 5
 sudo ln -s ~/panda/board/ /usr/local/lib/python3.9/dist-packages/pandacan-0.0.10-py3.9.egg/board
 sudo ./recover.py
 sleep 10
