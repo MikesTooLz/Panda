5e76de669f01954691fe89e563e10a03f9daf13c
MikesTooLz <github.track@mikestoolz.com>
2023-10-08 16:16:56 -0400

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
-apt-get update
-apt-get install dfu-util gcc-arm-none-eabi python3-pip libffi-dev git scons screen
-sleep 5
+sudo apt-get update
+sudo apt-get install dfu-util gcc-arm-none-eabi python3-pip libffi-dev git scons screen
 git clone https://github.com/commaai/panda.git
-sleep 5
 cd panda
 pip install -r requirements.txt
-sleep 5
-python setup.py install
-sleep 5
+sudo python setup.py install
 grep SUBSYS README.md | sudo tee /etc/udev/rules.d/11-panda.rules
-udevadm control --reload-rules && sudo udevadm trigger
+sudo udevadm control --reload-rules && sudo udevadm trigger
 lsusb
 cd board
-sudo scons -u
-sleep 5
-sudo ln -s ~/panda/board/ /usr/local/lib/python3.9/dist-packages/pandacan-0.0.10-py3.9.egg/board
-sudo ./recover.py
+scons -u
 sleep 10
-sudo ./flash.py
+sudo ln -s ~/panda/board/ /usr/local/lib/python3.9/dist-packages/pandacan-0.0.10-py3.9.egg/board
+./recover.py
+sleep 10
+./flash.py
 cd ..
 cd ..
 wget -O MarsMode.py https://github.com/MikesTooLz/Panda/raw/main/marsmode-mediavolume-basic.py
