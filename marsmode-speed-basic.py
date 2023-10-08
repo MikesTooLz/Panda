8e00ed019d436f840824462c467fe720a68cbab5
MikesTooLz <github.track@mikestoolz.com>
2023-10-08 14:36:34 -0400

    Create marsmode-speed-basic.py

---
+++ marsmode-speed-basic.py
@@ -0,0 +1,21 @@
+#marsmode-speed-basic by @spleck
+
+import binascii
+import time
+import random
+from panda import Panda
+
+p = Panda()
+
+p.set_scan_speed_kbps(0,500)
+p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+
+while True:
+        try:
+                p.can_send(0x3c2,b"\x29\x55\x00\x01\x00\x00\x00\x00",0) # speed Down
+                time.sleep(0.3)
+                p.can_send(0x3c2,b"\x29\x55\x00\x3F\x00\x00\x00\x00",0) # speed Up
+                time.sleep(4+random.uniform(0,4))
+        except Exception as e:
+                print("Exception caught ",e)
+                time.sleep(3.2)
