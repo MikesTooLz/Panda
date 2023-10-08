e6c67e3b41800c830dba04753afc925eadfb6967
MikesTooLz <github.track@mikestoolz.com>
2023-10-08 11:24:00 -0400

    Create Setup.sh
    Initial setup of pi and panda

---
+++ Setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt-get update
+sudo apt-get update
+sudo apt-get install dfu-util gcc-arm-none-eabi python3-pip libffi-dev git scons screen
+git clone https://github.com/commaai/panda.git
+cd panda
+pip install -r requirements.txt
+sudo python setup.py install
+grep SUBSYS README.md | sudo tee /etc/udev/rules.d/11-panda.rules
+sudo udevadm control --reload-rules && sudo udevadm trigger
+lsusb
+cd board
+scons -u
+./recover.py
+sudo ln -s ~/panda/board/ /usr/local/lib/python3.9/dist-packages/pandacan-0.010-py3.9.egg/board
+./recover.py
+./flash.py
